ff763624f36bd27cae11a4126b3870d06eed674d
qxr <qxr@mangatoon.mobi>
2022-01-06 14:58:45 +0800

    pod lib for demo

---
+++ PodTestXl/Classes/TestTool.h
@@ -0,0 +1,18 @@
+//
+//  TestTool.h
+//  PodTestXl
+//
+//  Created by 邱笑然 on 2022/1/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestTool : NSObject
+
+- (void)testLog;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PodTestXl/Classes/TestTool.m
@@ -0,0 +1,16 @@
+//
+//  TestTool.m
+//  PodTestXl
+//
+//  Created by 邱笑然 on 2022/1/6.
+//
+
+#import "TestTool.h"
+
+@implementation TestTool
+
+- (void)testLog {
+  NSLog(@"pod test");
+}
+
+@end
